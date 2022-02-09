c4e218b3d00c9064ad3c52a6d61faed88146042d
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-09 18:53:23 +0800

    allow search bar to open search page

--- scripts/index.js
+++ scripts/index.js
@@ -107,3 +107,12 @@ async function load() {
     await loadCategories();
     await loadPopularItems();
 }
+
+function checkEnter(event) {
+    if (event.keyCode == 13) {
+        var text = document.getElementById('search').value;
+
+        window.open(`search?query=${text}`,"_self")
+        return false;
+    }
+}
