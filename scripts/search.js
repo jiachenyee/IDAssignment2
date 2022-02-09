51183c452bbfb73a070d326c4d0f55ba329276c8
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-09 19:21:08 +0800

    add ability to edit query within search page

--- scripts/search.js
+++ scripts/search.js
@@ -1,13 +1,12 @@
 const params = new URLSearchParams(window.location.search);
 const query = params.get('query').toLowerCase();
+document.getElementById('search').value = query;
+var productsList = []
 
 async function load() {
     var productData = await loadProductData();
-    var productsList = productData.flatMap(category => category["products"]);
-
-    var searchResults = productsList.filter(product => query.includes(product["name"].toLowerCase()) || product["name"].toLowerCase().includes(query))
-
-    console.log(searchResults);
+    productsList = productData.flatMap(category => category["products"]);
+    displayResults(query, productsList);
 }
 
 async function loadProductData() {
@@ -20,4 +19,19 @@ async function loadProductData() {
         .then(data => productData = data);
 
     return productData;
+}
+
+function checkEnter(event) {
+    if (event.keyCode == 13) {
+        var text = document.getElementById('search').value;
+
+        window.open(`search?query=${text}`,"_self")
+
+        return false;
+    }
+}
+
+function displayResults(query, productList) {
+    var searchResults = productList.filter(product => query.includes(product["name"].toLowerCase()) || product["name"].toLowerCase().includes(query))
+    console.log(searchResults);
 }
