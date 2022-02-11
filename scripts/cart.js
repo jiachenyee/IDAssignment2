b7bf5a6c7ef23bf6f90ffa567ecc667441079cc8
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-12 02:25:22 +0800

    add search to cart

---
+++ scripts/cart.js
@@ -0,0 +1,8 @@
+function checkEnter(event) {
+    if (event.keyCode == 13) {
+        var text = document.getElementById('search').value;
+
+        window.open(`search?query=${text}`,"_self")
+        return false;
+    }
+}
