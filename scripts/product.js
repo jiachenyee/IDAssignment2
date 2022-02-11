081eacc1cade59e3971f4b9c05d1b34f65febe59
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-12 02:16:43 +0800

    add js behind search in product page

--- scripts/product.js
+++ scripts/product.js
@@ -70,3 +70,12 @@ async function loadTagData() {
 
     return tags;
 }
+
+function checkEnter(event) {
+    if (event.keyCode == 13) {
+        var text = document.getElementById('search').value;
+
+        window.open(`search?query=${text}`,"_self")
+        return false;
+    }
+}
