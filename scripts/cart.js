d4c004cabc77b3d4816135f485dacc1af9cbe8fd
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-12 21:11:50 +0800

    load products from cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,21 @@
+var cartContents = localStorage.getItem("cart");
+
+// Add placeholder cart contents
+cartContents = [
+    {
+        "sku": "PAST009",
+        "price": 0,
+        "qty": 1,
+        "editableQty": true
+    },
+    {
+        "sku": "PAST010",
+        "price": 10,
+        "qty": 1,
+        "editableQty": true
+    }
+]
+
 function checkEnter(event) {
     if (event.keyCode == 13) {
         var text = document.getElementById('search').value;
@@ -5,4 +23,22 @@ function checkEnter(event) {
         window.open(`search?query=${text}`,"_self")
         return false;
     }
+}
+
+async function load() {
+    var productData = await loadProductData();
+
+    console.log(cartContents)
+}
+
+async function loadProductData() {
+    var productData = [];
+
+    await fetch("./resources/categories.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => productData = data);
+
+    return productData;
 }
