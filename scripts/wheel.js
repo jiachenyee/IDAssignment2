d4941e980722711d48eaf5072261412e46426d3a
Ryanyzh <S10219317@connect.np.edu.sg>
2022-02-13 17:28:21 +0800

    add overlay to wheel game

--- scripts/wheel.js
+++ scripts/wheel.js
@@ -1,4 +1,5 @@
 function myfunction(){
+    calculateSpins();
     var x = 1024; //min value
     var y = 9999; //max-value
   
@@ -13,23 +14,44 @@ function myfunction(){
       displayEle();
     }, 5000);
 
-    
+    setTimeout(function(){
+      overlay();
+    }, 5500);    
   }
 
 
-let posX = (window.innerWidth / 1.5);
+let posX = (window.innerWidth / 1.7);
 let posY = (window.innerHeight / 2.0);
+let overlayEle = document.getElementById("overlay");
+let rewards = document.getElementById("rewards");
+let title = document.getElementById("title");
+let spins = 2;
 
 function displayEle(){
-  var ele = document.elementFromPoint(posX, posY);
+  let qty = 1;
+  var ele = document.elementFromPoint(posX, posY).textContent;
+  rewards.textContent = ele + " x " + qty;
   console.log("break");
   console.log(posX);
   console.log(posY);
   console.log(ele);
 }
 
+function again(){
+  overlayEle.style.display = "none";
+}
+
+function overlay(){
+  overlayEle.style.display = "flex";
+}
+
+function calculateSpins(){
+  spins -= 1;
+  title.textContent = "Spin(s) left: " + spins;
+}
 
 
-document.addEventListener("mousemove", ({pageX, pageY}) => {
+let pos;
+document.addEventListener("mousemove", ({pageX , pageY}) => {
   pos = `(${pageX}, ${pageY})`;
-});
+})
