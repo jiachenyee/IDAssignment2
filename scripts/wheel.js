05a15aa1e91c61ca142a0aaae516e23812bc5d70
Ryanyzh <S10219317@connect.np.edu.sg>
2022-02-13 15:27:39 +0800

    determining position for wheel game

--- scripts/wheel.js
+++ scripts/wheel.js
@@ -10,8 +10,26 @@ function myfunction(){
     element.classList.remove('animate');
     setTimeout(function(){
       element.classList.add('animate');
+      displayEle();
     }, 5000);
+
+    
   }
 
 
-  
+let posX = (window.innerWidth / 1.5);
+let posY = (window.innerHeight / 2.0);
+
+function displayEle(){
+  var ele = document.elementFromPoint(posX, posY);
+  console.log("break");
+  console.log(posX);
+  console.log(posY);
+  console.log(ele);
+}
+
+
+
+document.addEventListener("mousemove", ({pageX, pageY}) => {
+  pos = `(${pageX}, ${pageY})`;
+});
