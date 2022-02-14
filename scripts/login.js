813d075e91eb9570228cb0448433b96a93eacfae
Ryanyzh <S10219317@connect.np.edu.sg>
2022-02-14 17:34:13 +0800

    add data validation to sign up form

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,3 @@
-//for status of login form
-function setFormMessage(formElement, type, message){
-    const messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message-success", "form__message-error");
-    messageElement.classList.add('form__message-${type}');
-}
-
 //for data validations
 function setInputError(inputElement, message){
     inputElement.classList.add("form__input-error");
@@ -23,9 +14,16 @@ function clearInputError(inputElement){
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    var validity = false;
-    
-    const createAccountForm1 = document.querySelector("#signup1");
+    var regExp = /[a-zA-Z]/g;
+    var regExp1 = /\d/;
+    let pwd = "";
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    let inputValidator = {
+      "usernameTF" : false,
+      "emailTF" : false,
+      "passwordTF" : false,
+      "confirmPwdTF" : false
+    }
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
       e.preventDefault();
@@ -34,14 +32,97 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id === "username" &&e.target.value.length > 0 && e.target.value.length < 8){
-                setInputError(inputElement, "Username must be at least 8 characters in length.");
+            //data validation for input fields
+
+            //username field
+            if (e.target.id === "username"){
+                if(e.target.value.length == 0){
+                    setInputError(inputElement, "Username is required.");
+                }
+                else if(e.target.value.length > 0 && e.target.value.length < 8){
+                    setInputError(inputElement, "Username must be at least 8 characters in length.");
+                }
+                else{
+                    let name = "usernameTF";
+                    inputValidator[name] = true;
+                }
             }
+
+            //email field
+            if (e.target.id === "emailaddr"){
+                if(e.target.value.length == 0){
+                    setInputError(inputElement, "Email is required.");
+                }
+                else if(!(e.target.value.includes("@") && e.target.value.includes("."))){
+                  setInputError(inputElement, "Email format is invalid (missing @ or .).");
+                }
+                else{
+                  let name = "emailTF";
+                  inputValidator[name] = true;
+                }
+            }
+
+            //password field
+            if (e.target.id === "password"){
+                if(e.target.value.length == 0){
+                    setInputError(inputElement, "Password is required.");
+                }
+                else if(e.target.value.length < 8){
+                    setInputError(inputElement, "Password must be at least 8 characters in length");
+                }
+                else if(!regExp.test(e.target.value)){
+                     setInputError(inputElement, "Password must contain at least a letter.");
+                }
+                else if(!regExp1.test(e.target.value)){
+                    setInputError(inputElement, "Password must contain at least a number");
+                }
+                else if(!specialChars.test(e.target.value)){
+                     setInputError(inputElement, "Password must contain at least a special character(symbols)");
+                }
+                else{
+                    let name = "passwordTF";
+                    inputValidator[name] = true;
+                }
+                pwd = e.target.value;
+            }
+
+
+            //confirm password field
+            if(e.target.id === "cfmpassword"){
+              if(e.target.value.length == 0){
+                  setInputError(inputElement, "Password is required.");
+              }
+              else if(e.target.value != pwd){
+                  setInputError(inputElement, "Password does not match the above");
+              }
+              else{
+                  let name = "confirmPwdTF";
+                  inputValidator[name] = true;
+              }
+            }
+
+
+            let allTrue = Object.keys(inputValidator).every((item) => {
+                return inputValidator[item] === true;
+            });
+    
+            console.log(inputValidator);
+    
+            if (allTrue) {
+                document.getElementById("next1").disabled = false;
+            }
+            else{
+                document.getElementById("next1").disabled = true;
+            }
+            
         });
 
         inputElement.addEventListener("input", e => {
             clearInputError(inputElement);
         });
+
+        
+
     });
 });
 
