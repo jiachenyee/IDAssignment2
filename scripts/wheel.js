f3a81fd22c47d25012f766e95388f5647fd132ee
Ryanyzh <S10219317@connect.np.edu.sg>
2022-02-16 17:04:43 +0800

    add products to local storage from wheel spin

--- scripts/wheel.js
+++ scripts/wheel.js
@@ -1,5 +1,22 @@
+var ele;
+let overlayEle = document.getElementById("overlay");
+let rewards = document.getElementById("rewards");
+let title = document.getElementById("title");
+
+
+document.addEventListener("DOMContentLoaded" , e => {
+    let spins = localStorage.getItem("spins");
+    document.getElementById("title").textContent = "Spin(s) Left: " + spins;
+    //localStorage.setItem("spins", spins);
+});
+    
+
+
 function myfunction(){
-    calculateSpins();
+    let spins = localStorage.getItem("spins");
+    //console.log(spins);
+    localStorage.setItem("spins" , spins-1);
+    document.getElementById("title").textContent = "Spin(s) Left: " + (spins-1);
     var x = 1024; //min value
     var y = 9999; //max-value
   
@@ -15,45 +32,51 @@ function myfunction(){
     }, 5000);
 
     setTimeout(function(){
-      overlay();
+        overlayEle.style.display = "flex";
     }, 5500);    
   }
 
 
 
 
-let overlayEle = document.getElementById("overlay");
-let rewards = document.getElementById("rewards");
-let title = document.getElementById("title");
-let spins = localStorage.getItem("spins");
-
 function displayEle(){
-  let posX;
-  if (window.innerWidth < 600){
-    posX = (window.innerWidth / 1.3);
-  }
-  else{
-    posX = (window.innerWidth / 1.7);
-  }
-  let posY = (window.innerHeight / 2.0);
-  let qty = 1;
-  var ele = document.elementFromPoint(posX, posY);
-  rewards.textContent = ele.textContent;
-  console.log(ele);
+    let posX;
+    if (window.innerWidth < 600){
+      posX = (window.innerWidth / 1.3);
+    }
+    else{
+      posX = (window.innerWidth / 1.7);
+    }
+    let posY = (window.innerHeight / 2.0);
+    let qty = 1;
+    ele = document.elementFromPoint(posX, posY);
+    rewards.textContent = ele.textContent;
+    console.log(ele);
 }
 
 function again(){
-  overlayEle.style.display = "none";
+    let spins = localStorage.getItem("spins");
+    if(spins <= 0){
+        window.open(`cart`,"_self");
+    }
+    else{
+        overlayEle.style.display = "none";
+        var cart = JSON.parse(localStorage.getItem("cart"));
+          
+        if (cart == undefined) {
+            cart = [];
+        }
+    
+        cart.push({
+            "sku": ele.id, // replace with SKU
+            "price": 0, // make it free
+            "qty": 1,   // give them 1
+            "editableQty": false // set this to false so users cannot add more
+        });
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
 }
 
-function overlay(){
-  overlayEle.style.display = "flex";
-}
-
-function calculateSpins(){
-  spins -= 1;
-  title.textContent = "Spin(s) left: " + spins;
-}
 
 
 let pos;
@@ -61,8 +84,7 @@ document.addEventListener("mousemove", ({pageX , pageY}) => {
   pos = `(${pageX}, ${pageY})`;
 })
 
-
-
+/*
 
 var cart = JSON.parse(localStorage.getItem("cart"));
     
@@ -79,3 +101,6 @@ cart.push({
 localStorage.setItem("cart", JSON.stringify(cart));
 
 window.open(`cart`,"_self")
+
+*/
+
