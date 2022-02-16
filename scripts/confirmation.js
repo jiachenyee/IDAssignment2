ab2d438f1f8b0ed486eb82ba7bee4fa4d0e48069
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-16 20:27:27 +0800

    add delivery date time to success page

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -1,2 +1,10 @@
 const params = new URLSearchParams(window.location.search);
 const dateString = params.get('date');
+
+var deliveryDateTime = document.getElementById("deliveryDateTime");
+
+var dateSegments = dateString.split("-");
+
+var date = new Date(dateSegments[2],dateSegments[1] - 1,dateSegments[0]);
+
+deliveryDateTime.innerText = "1pm and 4pm on " + date.toDateString();
